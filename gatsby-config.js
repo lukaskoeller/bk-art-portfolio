6a9775f35e778dc2116c08a4f184218e0bb27c37
lukaskoeller <lukas-koeller@onlinehome.de>
2020-03-17 18:36:26 +0100

    Added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,13 +57,13 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'barbara koeller portfolio',
+        short_name: 'portfolio',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#ffffff',
+        theme_color: '#000000',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+        icon: 'src/images/barbara-koeller-logo.png' // This path is relative to the root of the site.
       }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
