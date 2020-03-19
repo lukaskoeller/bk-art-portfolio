77b3ecad3ebe3e022c565809531e86962e907071
lukaskoeller <lukas-koeller@onlinehome.de>
2020-03-19 17:07:50 +0100

    Adding styling to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,24 +1,35 @@
-// import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
+import React from 'react'
 
 // Components
-import Container from "../components/container"
+import Container from '../components/container'
+import Logo from '../images/barbara-koeller-logo.inline.svg'
+
+// Styles
+import './footer.scss'
 
 const Footer = ({ siteTitle }) => (
-  <Container>
-    <footer>
-      {siteTitle} - Year {new Date().getFullYear()}
+  <Container styleModifier={['dark-theme']}>
+    <footer className="footer">
+      <Link to="/">
+        <Logo />
+      </Link>
+      <div className="footer__notes">
+        <span className="footer__item">Liebe Grüße aus Hamburg. {siteTitle}</span>
+        <Link to="/imprints" className="footer__item">Impressum</Link>
+        <Link to="/privacy" className="footer__item">Datenschutzerklärung</Link>
+      </div>
     </footer>
   </Container>
 )
 
 Footer.propTypes = {
-  siteTitle: PropTypes.string,
+  siteTitle: PropTypes.string
 }
 
 Footer.defaultProps = {
-  siteTitle: ``,
+  siteTitle: ''
 }
 
 export default Footer
